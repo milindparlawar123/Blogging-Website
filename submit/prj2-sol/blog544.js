3892b648f6e378804f27a628afc64ba3b9710064
Milind Keshav Parlawar <mparlaw1@CS444-mparlaw1.pods.bu.int>
2020-03-10 17:48:42 -0400

    file changes completed

--- submit/prj2-sol/blog544.js
+++ submit/prj2-sol/blog544.js
@@ -1,7 +1,6 @@
 // -*- mode: JavaScript; -*-
 
 import mongo from 'mongodb';
-
 import BlogError from './blog-error.js';
 import Validator from './validator.js';
 
@@ -44,8 +43,10 @@ MISSING_FIELD:
 
 export default class Blog544 {
 
-  constructor(meta, options) {
+  constructor(meta, options, client, db) {
     //@TODO
+    this.client = client;
+    this.db = db;
     this.meta = meta;
     this.options = options;
     this.validator = new Validator(meta);
@@ -54,7 +55,9 @@ export default class Blog544 {
   /** options.dbUrl contains URL for mongo database */
   static async make(meta, options) {
     //@TOD
-    return new Blog544(meta, options);
+    const client = await mongo.connect(options.dbUrl, MONGO_CONNECT_OPTIONS);
+    const db = client.db(DB_NAME);
+    return new Blog544(meta, options, client, db);
   }
 
   /** Release all resources held by this blog.  Specifically, close
@@ -62,19 +65,90 @@ export default class Blog544 {
    */
   async close() {
     //@TODO
+    this.client.close();
   }
 
   /** Remove all data for this blog */
   async clear() {
     //@TODO
+
+    await this.db.collection('data.users').deleteMany({});
+    await this.db.collection('data.articles').deleteMany({});
+    await this.db.collection('data.comments').deleteMany({});
+
   }
 
   /** Create a blog object as per createSpecs and 
    * return id of newly created object 
    */
   async create(category, createSpecs) {
+
     const obj = this.validator.validate(category, 'create', createSpecs);
     //@TODO
+    const errors = [];
+    if (obj._id) {
+      errors.push(new BlogError('BAD_FIELD', ' the internal mongo _id field is forbidden for ' + category + ' create'));
+      if (errors.length > 0) throw errors;
+    }
+    if (category == 'users') {
+      var array = await this.find(category, { id: createSpecs.id });
+      if (array && array.length > 0) {
+        errors.push(new BlogError('BAD_ID', ' users object having id ' + obj.id + ' is  already exists  '));
+
+      } else {
+
+        try {
+          await this.db.collection("data.users").insertOne(obj);
+
+          var count = await this.db.collection("data.users").countDocuments();
+          if (count == 1) {
+            await this.createIndexes(category);
+          }
+          return obj.id;
+        } catch (error) {
+          errors.push(new BlogError('', error));
+        }
+      }
+    }
+    if (category == 'articles') {
+      var array = await this.find(category, { id: createSpecs.id });
+      if (array && array.length > 0) {
+        errors.push(new BlogError('BAD_ID', 'articles object having id  ' + obj.id + ' is already exists  '));
+      } else {
+        try {
+          var ran = (Math.random() * 1000).toString().substr(0, 9);
+          obj.id = ran;
+          await this.db.collection("data.articles").insertOne(obj);
+          var count = await this.db.collection("data.articles").countDocuments();
+          if (count == 1) {
+            await this.createIndexes(category);
+          }
+          return ran;
+        } catch (error) {
+          errors.push(new BlogError('', error));
+        }
+      }
+    }
+    if (category == 'comments') {
+      var array = await this.find(category, { id: createSpecs.id });
+      if (array && array.length > 0) {
+        errors.push(new BlogError('BAD_ID', 'comments object having id  ' + obj.id + ' is already exists  '));
+      } else {
+        try {
+          var ran = (Math.random() * 1000).toString().substr(0, 9);
+          obj.id = ran;
+          await this.db.collection("data.comments").insertOne(obj);
+          var count = await this.db.collection("data.comments").countDocuments();
+          if (count == 1) {
+            await this.createIndexes(category);
+          }
+          return ran;
+        } catch (error) {
+          errors.push(new BlogError('', error));
+        }
+      }
+    }
+    if (errors.length > 0) throw errors;
   }
 
   /** Find blog objects from category which meets findSpec.  
@@ -92,16 +166,92 @@ export default class Blog544 {
    *    ...
    *  
    */
-  async find(category, findSpecs={}) {
+  async find(category, findSpecs = {}) {
     const obj = this.validator.validate(category, 'find', findSpecs);
     //@TODO
-    return [];
+
+    const errors = [];
+    if (obj._id) {
+      errors.push(new BlogError('BAD_FIELD', ' the internal mongo _id field is forbidden for ' + category + ' find'));
+      if (errors.length > 0) throw errors;
+    }
+    if (findSpecs.creationTime) {
+      findSpecs.creationTime = { $lte: new Date(findSpecs.creationTime) };
+    }
+    if (findSpecs._index) {
+      delete findSpecs._index;
+    }
+    if (findSpecs._count) {
+      delete findSpecs._count;
+    }
+    var objectsArr = [];
+
+      try {
+
+        const ret = await this.db.collection("data." + category).find(findSpecs, { projection: { _id: 0 } }).skip(obj._index ? parseInt(obj._index) : 0).limit(obj._count ? parseInt(obj._count) : DEFAULT_COUNT).sort([['creationTime', -1]]);
+        objectsArr = await ret.toArray();
+
+      } catch (error) {
+        errors.push(new BlogError('', error));
+      }
+
+    if (errors.length > 0) throw errors;
+    return objectsArr;
   }
 
   /** Remove up to one blog object from category with id == rmSpecs.id. */
   async remove(category, rmSpecs) {
     const obj = this.validator.validate(category, 'remove', rmSpecs);
+    const errors = [];
+    if (obj._id) {
+      errors.push(new BlogError('BAD_FIELD', ' the internal mongo _id field is forbidden for ' + category + ' remove'));
+      if (errors.length > 0) throw errors;
+    }
+    if (category == 'users') {
+      var array = await this.find(category, rmSpecs);
+      if (!array || array.length == 0) {
+        errors.push(new BlogError('BAD_ID', 'no users for id ' + obj.id + ' in remove  '));
+      }
+      else {
+
+        var articlesaArray = await this.getUsersIdOfArticles('articles', { authorId: rmSpecs.id });
+        var commentsaArray = await this.getUsersIdOfComments('comments', { commenterId: rmSpecs.id });
+        if (articlesaArray)
+          errors.push(new BlogError('BAD_ID', 'users ' + obj.id + ' referenced by authorId for articles ' + articlesaArray));
+        if (commentsaArray)
+          errors.push(new BlogError('BAD_ID', 'users ' + obj.id + ' referenced by commenterId for comments ' + commentsaArray));
+
+        if (errors.length == 0) {
+          await this.db.collection('data.users').deleteOne({ id: rmSpecs.id });
+        }
+      }
+
+    }
+    if (category == 'articles') {
+      var array = await this.find(category, rmSpecs);
+      if (!array || array.length == 0) {
+        errors.push(new BlogError('BAD_ID', 'no articles for id ' + obj.id + ' in remove  '));
+      }
+      else {
+        var commentsaArray = await this.getUsersIdOfComments('comments', { articleId: rmSpecs.id });
+        if (commentsaArray) {
+          errors.push(new BlogError('BAD_ID', 'articles ' + obj.id + ' referenced by articleId for comments ' + commentsaArray));
+        }
+        else {
+          await this.db.collection('data.articles').deleteOne({ id: rmSpecs.id });
+        }
+      }
+    }
+    if (category == 'comments') {
+      var array = await this.find(category, rmSpecs);
+      if (!array || array.length == 0) {
+        errors.push(new BlogError('BAD_ID', 'no comments for id ' + obj.id + ' in remove  '));
+      } else {
+        await this.db.collection('data.comments').deleteOne({ id: rmSpecs.id });
+      }
+    }
     //@TODO
+    if (errors.length > 0) throw errors;
   }
 
   /** Update blog object updateSpecs.id from category as per
@@ -110,10 +260,117 @@ export default class Blog544 {
   async update(category, updateSpecs) {
     const obj = this.validator.validate(category, 'update', updateSpecs);
     //@TODO
-  }
+    const errors = [];
+    if (obj._id) {
+      errors.push(new BlogError('BAD_FIELD', ' the internal mongo _id field is forbidden for ' + category + ' update'));
+      if (errors.length > 0) throw errors;
+    }
+   
+      var array = await this.findForUpdate(category, { id: updateSpecs.id });
+      if (array && array.length > 0) {
+        var updateObj = array[0];
+
+        for (let [key, value] of Object.entries(updateSpecs)) {
+          if (key != 'id') {
+            updateObj[key] = value;
+          }
+        }
+        updateObj.updateTime = new Date();
+
+        try {
+          await this.db.collection("data." + category).updateOne({ _id: updateObj._id }, { $set: updateObj });
+        } catch (error) {
+          errors.push(new BlogError('', error));
+        }
+
+      } else {
+
+        errors.push(new BlogError('BAD_ID', ' ' + category + ' object having id ' + obj.id + ' is  not exists  '));
+
+      }
   
+    if (errors.length > 0) throw errors;
+  }
+
+  async getUsersIdOfArticles(category, rmSpecs) {
+    const obj = this.validator.validate(category, 'find', rmSpecs);
+    //@TODO
+    var errors = [];
+    var objectsArr = [];
+    try {
+      const ret = await this.db.collection("data." + category).find(rmSpecs, { projection: { id: 1, _id: 0 } });
+      objectsArr = await ret.toArray();
+      return objectsArr.map(d => d.id).toString();
+    } catch (error) {
+      errors.push(new BlogError('', error));
+    }
+    if (errors.length > 0) throw errors;
+  }
+  async getUsersIdOfComments(category, rmSpecs) {
+    const obj = this.validator.validate(category, 'find', rmSpecs);
+    //@TODO
+    var errors = [];
+    var objectsArr = [];
+    try {
+      const ret = await this.db.collection("data." + category).find(rmSpecs, { projection: { id: 1, _id: 0 } });
+      objectsArr = await ret.toArray();
+      return objectsArr.map(d => d.id).toString();
+    } catch (error) {
+      errors.push(new BlogError('', error));
+    }
+    if (errors.length > 0) throw errors;
+  }
+
+  async createIndexes(categoryInput) {
+    for (const [category, infos] of Object.entries(this.meta)) {
+
+      if (category == categoryInput) {
+        const temp = [];
+        infos.filter(info => info.doIndex == true).map(info => temp[info.name] = 1);
+
+        for (const t in temp) {
+          var tObj = {};
+          tObj[t] = temp[t];
+          if (tObj.creationTime) {
+            tObj.creationTime = -1;
+          }
+          await this.db.collection("data." + category).createIndex(tObj);
+
+        }
+      }
+    }
+  }
+
+  async findForUpdate(category, findSpecs = {}) {
+    const obj = this.validator.validate(category, 'find', findSpecs);
+    //@TODO
+    const errors = [];
+    if (findSpecs.creationTime) {
+      findSpecs.creationTime = { $lte: new Date(findSpecs.creationTime) };
+    }
+    if (findSpecs._index) {
+      delete findSpecs._index;
+    }
+    if (findSpecs._count) {
+      delete findSpecs._count;
+    }
+    var objectsArr = [];
+
+      try {
+        const ret = await this.db.collection("data." + category).find(findSpecs).skip(obj._index ? parseInt(obj._index) : 0).limit(obj._count ? parseInt(obj._count) : DEFAULT_COUNT).sort([['creationTime', -1]]);
+        objectsArr = await ret.toArray();
+
+      } catch (error) {
+        errors.push(new BlogError('', error));
+      }
+
+    if (errors.length > 0) throw errors;
+
+    return objectsArr;
+  }
+
 }
 
 const DEFAULT_COUNT = 5;
-
+const DB_NAME = 'blog';
 const MONGO_CONNECT_OPTIONS = { useUnifiedTopology: true };
